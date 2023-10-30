f58fcd1c0ec1cab2ceb5079738ada955fd6e5cd4
LLucas587 <lilucas587@gmail.com>
2023-10-30 14:23:10 -0400

    Added Api Key + Comments

--- src/data_access/FdaDatabaseAccessObject.java
+++ src/data_access/FdaDatabaseAccessObject.java
@@ -1,2 +1,10 @@
-package data_access;public class FdaDatabaseAccessObject {
+package data_access;
+public class FdaDatabaseAccessObject {
+    String apiKey = "AhsIyByI3vFVW2gRIsMVT9u4UbAgGTwgnQxxHPZi";
+    String baseCall = "https://api.fda.gov/drug/drugsfda.json";
+
+    //Add Drug Alerts
+    //Add Recall Alerts
+    //Add Drug Info Call
+
 }
