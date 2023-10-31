2b400168e4e69adbe7c7884c45054e28cad5c6b5
LLucas587 <lilucas587@gmail.com>
2023-10-31 16:32:58 -0400

    Added recentlyRecalled function to FdaDataBaseAccessObject

--- src/data_access/CSVDatabaseAccessObject.java
+++ src/data_access/CSVDatabaseAccessObject.java
@@ -3,18 +3,22 @@ import entity.User;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CSVDatabaseAccessObject {
-    private final File csvFile;
+    private File csvFile;
     private final String[] patient_headers = {"name", "password", "medication"};
     private Map<String, User> patients = new HashMap<>();
 
-    public CSVDatabaseAccessObject(String file_path) throws IOException{
+    public CSVDatabaseAccessObject(File csvFile) {
+        this.csvFile = csvFile;
+    }
+
+    public Map<String, User> CSVDatabaseAccessObject(String file_path) throws IOException{
         csvFile = new File(file_path);
         if (csvFile.length() == 0) {
             return patients;
         }
+        return null;
     }
 }
--- src/data_access/FdaDatabaseAccessObject.java
+++ src/data_access/FdaDatabaseAccessObject.java
@@ -1,28 +1,65 @@
 package data_access;
 import java.util.List;
+import java.util.ArrayList;
 import java.net.*;
 import java.io.*;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class FdaDatabaseAccessObject {
-    String apiKey = "AhsIyByI3vFVW2gRIsMVT9u4UbAgGTwgnQxxHPZi";
-    String baseCall = "https://api.fda.gov/drug/drugsfda.json";
+    String apiKey ;
+    String baseCall;
+
+    public FdaDatabaseAccessObject(){
+        apiKey = "AhsIyByI3vFVW2gRIsMVT9u4UbAgGTwgnQxxHPZi";
+        baseCall = "https://api.fda.gov/drug/drugsfda.json";
+    }
+
 
     //Add Drug Alerts
     //Add Recall Alerts
-    //Add Recently Recalled Drugs
-    public List<String> recentlyRecalled() throws IOException{
-
-        String apiCall = "https://api.fda.gov/drug/enforcement.json?api_key="+apiKey+"&limit=5";
-        URL url = new URL(apiCall);
-        URLConnection connection = url.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        connection.getInputStream()));
-        String inputLine;
-
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+    public List<Map<String, Object>> recentlyRecalled(Integer limit) throws IOException{
+        /**
+         * Returns a List of maps, with each item in the list being a map of information about a singular product recall
+         * @param  limit  the number of recent product recalls wanted
+         */
+        try {
+            // Building Json from Api Call
+            String apiCall = "https://api.fda.gov/drug/enforcement.json?api_key="+apiKey+"&limit="+limit.toString();
+            URL url = new URL(apiCall);
+            URLConnection connection = url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder jsonData = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonData.append(line);
+            }
+            String json = jsonData.toString();
+            reader.close();
+            // Converting json data into a list of maps
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(json);
+            JsonNode resultsArray = jsonNode.get("results");
+            List<Map<String, Object>> listOfMaps = new ArrayList<>();
+            for (JsonNode result : resultsArray) {
+                Map<String, Object> resultMap = objectMapper.convertValue(result, Map.class);
+                listOfMaps.add(resultMap);
+            }
+            return listOfMaps;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
     //Add Drug Info Call
+
+    public static void main(String[] args) throws IOException {
+        //Sample calls
+        FdaDatabaseAccessObject obj = new FdaDatabaseAccessObject();
+        System.out.println(obj.recentlyRecalled(5).get(0).get("Country"));
+    }
 
 }
