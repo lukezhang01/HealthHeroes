3976f3c4f55171cd600074c4139af3e5c65e14f2
LLucas587 <lilucas587@gmail.com>
2023-10-31 16:38:34 -0400

    Revert "Added recentlyRecalled function to FdaDataBaseAccessObject"
    This reverts commit 2b400168e4e69adbe7c7884c45054e28cad5c6b5.

--- src/data_access/CSVDatabaseAccessObject.java
+++ src/data_access/CSVDatabaseAccessObject.java
@@ -3,22 +3,18 @@ import entity.User;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CSVDatabaseAccessObject {
-    private File csvFile;
+    private final File csvFile;
     private final String[] patient_headers = {"name", "password", "medication"};
     private Map<String, User> patients = new HashMap<>();
 
-    public CSVDatabaseAccessObject(File csvFile) {
-        this.csvFile = csvFile;
-    }
-
-    public Map<String, User> CSVDatabaseAccessObject(String file_path) throws IOException{
+    public CSVDatabaseAccessObject(String file_path) throws IOException{
         csvFile = new File(file_path);
         if (csvFile.length() == 0) {
             return patients;
         }
-        return null;
     }
 }
