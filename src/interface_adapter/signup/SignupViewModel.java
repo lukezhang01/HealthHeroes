3caf29afb71d1436af996a556271956b07a78475
lukez999 <zluke7111@gmail.com>
2023-11-16 17:16:11 -0500

    added view model

--- src/interface_adapter/signup/SignupViewModel.java
+++ src/interface_adapter/signup/SignupViewModel.java
@@ -1,5 +1,34 @@
 package interface_adapter.signup;
 
-public class SignupViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SignupViewModel extends ViewModel {
     private SignupState state = new SignupState();
+
+    public SignupViewModel() {
+        super("sign up");
+    }
+
+    public void setState(SignupState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public SignupState getState() {
+        return state;
+    }
 }
